5c7473a580e85083b9e01b312f130a1aa83d2f5f
kamrangul87 <kamrangul87@gmail.com>
2025-12-10 06:16:36 +0000

    Create clean_data.py

---
+++ ml/scripts/clean_data.py
@@ -0,0 +1,8 @@
+# Temporary test script for Autodun MOT Predictor
+# Later this file will clean the DVSA MOT data.
+
+def main():
+    print("clean_data.py is running...")
+
+if __name__ == "__main__":
+    main()
