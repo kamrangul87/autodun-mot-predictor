4a74be78c08198ef0bee4daa9cafaabed58988ee
kamrangul87 <kamrangul87@gmail.com>
2025-12-18 07:32:25 +0000

    Update mot-history.js

--- api/mot-history.js
+++ api/mot-history.js
@@ -90,7 +90,7 @@ export default async function handler(req, res) {
       headers: {
         "Accept": "application/json",
         "Authorization": `Bearer ${token}`,
-        "x-api-key": apiKey,
+        "X-API-Key": apiKey,
       },
     });
 
